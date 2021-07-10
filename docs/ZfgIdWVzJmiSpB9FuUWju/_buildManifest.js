b3e3214b6fc08e10975bfde3ad1dfa82a3b29ea9
Muhamed Muminovic <muminovic.muhamed01@gmail.com>
2021-07-10 22:31:44 +0200

    some unnecessary files deleted

--- docs/ZfgIdWVzJmiSpB9FuUWju/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/196-1de74b5ff51b2300fcaf.js","static/chunks/pages/index-bcedab289e349d367043.js"],"/_error":["static/chunks/pages/_error-82a806cd39f8ab3dc3ac.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
